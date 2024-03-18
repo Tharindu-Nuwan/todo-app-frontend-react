c22c4376dc103aa30cb0dd942fa87e7859947371
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-18 15:49:18 +0530

    Implement Tags Styles

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,8 +1,8 @@
 import "./Task.css";
 
 interface TagType {
-  id: number,
-  tag_name: string
+  id: number;
+  tag_name: string;
 }
 
 interface TaskProps {
@@ -11,34 +11,37 @@ interface TaskProps {
   tags: TagType[];
 }
 
-const tagColors = [
-  'blueviolet',
-  'blue',
-  'orangered',
-  'green'
-]
+const tagColors = ["blueviolet", "blue", "orangered", "green"];
 
 function Task({ title, description, tags }: TaskProps) {
-
-  const renderColorDots = ()=> {
-    return tags.map((tag)=> {
-      const color = tagColors[tag.id-1];
+  const renderColorDots = () => {
+    return tags.map((tag) => {
+      const color = tagColors[tag.id - 1];
       if (color) {
-        return <div key={tag.id} className={`${color} tag-dot`}></div>
+        return <div key={tag.id} className={`${color} tag-dot`}></div>;
       } else {
         return null;
-      }   
+      }
     });
-  }
-  
+  };
+
   return (
     <>
-      <div className="task d-flex flex-column align-items-center">
-      <div className="tag-dots-container">
+      <div className="task d-flex flex-column justify-content-between">
+        <div>
+        <div>
+          <h4>{title}</h4>
+          <h6>{description}</h6>
+        </div>
+
+        <div className="tag-dots-container d-flex flex-column">
           {renderColorDots()}
         </div>
-        <h4>{title}</h4>
-        <h6>{description}</h6>
+        </div>
+        <div className="d-flex flex-row-reverse gap-3">
+          <i className="bi bi-pencil-square fs-4"></i>
+          <i className="bi bi-trash3 fs-4"></i>
+        </div>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
-import Home from './components/home/Home'
+
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app-routes'
+import "bootstrap-icons/font/bootstrap-icons.min.css"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
