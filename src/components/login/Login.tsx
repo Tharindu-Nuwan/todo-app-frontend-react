3473390dae0a1b1f429903711f785eb56f494bb0
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-03-19 11:08:08 +0530

    Design Register Form

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -18,8 +18,8 @@ function Login() {
       });
       if (response.status === 200) {
         const { token } = response.data;
-        localStorage.setItem('token', token);
-        navigate('../app/home');
+        localStorage.setItem("token", token);
+        navigate("../app/home");
         Swal.fire({
           position: "center",
           icon: "success",
@@ -27,7 +27,6 @@ function Login() {
           showConfirmButton: false,
           timer: 2500,
         });
-      
       } else {
         Swal.fire({
           position: "center",
@@ -35,9 +34,9 @@ function Login() {
           title: "Error!",
           text: "Email and Password doesn't match with registered users!",
           showConfirmButton: false,
-          timer: 2000
+          timer: 2000,
         });
-        localStorage.removeItem('token')
+        localStorage.removeItem("token");
       }
     } catch (error) {
       alert(error);
@@ -48,52 +47,47 @@ function Login() {
     <>
       <form className="d-flex flex-column vw-100 vh-100 justify-content-center align-items-center">
         <div className="rounded p-5 shadow-lg mb-5 bg-body-tertiary d-flex justify-content-center align-items-center">
-        <div className="image-container">
-          </div>
+          <div className="image-container"></div>
           <div className="d-flex flex-column justify-content-center">
-          
-          <span className="mb-4 fs-2 text-warning">Login</span>
+            <span className="mb-4 fs-2 text-warning">Login</span>
 
-          <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="email">
-              Email Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="form-control"
-              placeholder="Enter your email here..."
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            
-          </div>
+            <div className="form-outline mb-4">
+              <label className="form-label" htmlFor="email">
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                placeholder="Enter your email here..."
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="form-outline mb-4">
-          <label className="form-label" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="form-control"
-              placeholder="Enter password here..."
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            
-          </div>
+            <div className="form-outline mb-4">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Enter password here..."
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <button
-            type="button"
-            className="btn btn-warning btn-block mb-4"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-          <p>
-            If you haven't registered yet? <Link to="/signup">Register</Link>
-          </p>
+            <button
+              type="button"
+              className="btn btn-warning btn-block mb-4"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+            <p>
+              If you haven't registered yet? <Link to="/signup">Register</Link>
+            </p>
           </div>
-          
         </div>
       </form>
     </>
--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react";
 import "./Signup.css";
 import axios from "axios";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 function Signup() {
@@ -19,15 +19,15 @@ function Signup() {
         password: password,
       });
 
-      if(response.status === 200) {
+      if (response.status === 200) {
         Swal.fire({
           position: "top-end",
           icon: "success",
           title: "You have been registered successfully",
           showConfirmButton: false,
-          timer: 2500
+          timer: 2500,
         });
-        navigate('/login');
+        navigate("/login");
       } else {
         Swal.fire({
           position: "top-end",
@@ -35,9 +35,9 @@ function Signup() {
           title: "Error!",
           text: "Enter valid email or password must contain minimum 4 charactors",
           showConfirmButton: false,
-          timer: 2000
+          timer: 2000,
         });
-      } 
+      }
     } catch (error) {
       alert(error);
     }
@@ -46,56 +46,64 @@ function Signup() {
   return (
     <>
       <form className="d-flex flex-column vw-100 vh-100 justify-content-center align-items-center">
-        <div className="rounded p-5 shadow-lg mb-5 bg-body-tertiary">
-          <h6 className="mb-4">Enter your credentials to register</h6>
-          <div className="form-outline mb-4 d-flex flex-column justify-content-center align-items-center">
-            <input
-              type="text"
-              id="name"
-              className="form-control text-center"
-              placeholder="Enter username here..."
-              onChange={(e) => setUsername(e.target.value)}
-            />
+        <div className="rounded p-5 shadow-lg mb-5 bg-body-tertiary d-flex justify-content-center align-items-center">
+          <div className="image-container"></div>
+          <div className="d-flex flex-column justify-content-center">
+          <span className="mb-4 fs-2 text-warning">Register</span>
+            <div className="form-outline mb-4">
             <label className="form-label" htmlFor="name">
-              Username
-            </label>
-          </div>
+                Username
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="form-control text-center"
+                placeholder="Enter username here..."
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              
+            </div>
 
-          <div className="form-outline mb-4 d-flex flex-column justify-content-center align-items-center">
-            <input
-              type="email"
-              id="email"
-              className="form-control text-center"
-              placeholder="Enter email here..."
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <div className="form-outline mb-4">
             <label className="form-label" htmlFor="email">
-              Email Address
-            </label>
-          </div>
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-control text-center"
+                placeholder="Enter email here..."
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              
+            </div>
 
-          <div className="form-outline mb-4 d-flex flex-column justify-content-center align-items-center">
-            <input
-              type="password"
-              id="password"
-              className="form-control text-center"
-              placeholder="Enter password here..."
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <div className="form-outline mb-4">
             <label className="form-label" htmlFor="password">
-              Password
-            </label>
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control text-center"
+                placeholder="Enter password here..."
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              
+            </div>
+            <div className="form-outline mb-4 d-flex flex-column justify-content-center align-items-center">
+              <button
+                type="button"
+                className="btn btn-warning btn-block mb-4"
+                onClick={handleSubmit}
+              >
+                Register
+              </button>
+              <p>
+                Already registered? <Link to="/login">Login</Link>
+              </p>
+            </div>
           </div>
-          <div className="form-outline mb-4 d-flex flex-column justify-content-center align-items-center">
-          <button
-            type="button"
-            className="btn btn-primary btn-block mb-4"
-            onClick={handleSubmit}
-          >
-            Register
-          </button>
-          <p>Already registered? <Link to="/login">Login</Link></p>
-        </div>
         </div>
       </form>
     </>
